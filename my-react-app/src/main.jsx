e0d1cf7068d8fe850f1fda459ca6e3d6268177aa
🐝 Mr. ßee - 🅾ng siêng năng📓 <122947792+MrBlackBeeOsn@users.noreply.github.com>
2025-09-20 01:54:59 +0000

    ye

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App3 from './App3.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App3 />
-  </StrictMode>,
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App3 />
+//   </StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/test/my-react-app/">
+    <App />
+  </BrowserRouter>
 )
